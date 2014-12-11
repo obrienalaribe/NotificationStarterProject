ed861a40f774e6b53990914b21b993dfdcacf434
Gabriel Theodoropoulos <gabriel@MISTHODOSIA-02.local>
2014-12-11 11:46:55 +0200

    Initial Commit

---
+++ Shopping Alert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shopping Alert
+//
+//  Created by Gabriel Theodoropoulos on 12/11/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
